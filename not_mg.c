e16ac62de874a3d29fa04880341187deb6cf09be
lemoin_a <lemoin_a@318c02d6-d926-42e8-9ef7-ea02efed1679>
2017-01-16 11:10:10 +0000

    Malloc map

--- not_mg.c
+++ not_mg.c
@@ -7,15 +7,24 @@ void my_putstr(char* str);
 int main() {
   t_room salle;
   FILE *fp;
+  int nb_line;
   char str[60];
-  int i = 0;
-  
+  int i;
+  char c;
+
+  i = 0;
+  nb_line = 0;
   fp = fopen("Maps/cargo_dock.map", "r");
   if(fp == NULL)
     {
       my_putstr("Erreur: cargo_dock.map introuvable");
       return 0;
     }
+  while ((c = getc(fp)) != EOF){
+    if (c == '\n')
+        ++nb_line;
+    }
+  salle.map = malloc(sizeof(str) * nb_line);
   while (fgets(str, 60, fp) != NULL)
     {
       salle.map[i] = str;
@@ -23,6 +32,6 @@ int main() {
       i++;
     }
   fclose(fp);
-  
+
   return 0;
 }
