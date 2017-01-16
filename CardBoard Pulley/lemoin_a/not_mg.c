07835df9d494ab38ab862b1a4393f832d55b4f11
lemoin_a <lemoin_a@318c02d6-d926-42e8-9ef7-ea02efed1679>
2017-01-16 10:51:54 +0000

    Ajout libmy static et struct room

--- CardBoard Pulley/lemoin_a/not_mg.c
+++ CardBoard Pulley/lemoin_a/not_mg.c
@@ -1,8 +1,28 @@
 #include <unistd.h>
-#include <stdlib.h>
-#include "libmy.a"
+#include <stdio.h>
+#include "room.h"
+
+void my_putstr(char* str);
 
 int main() {
-  my_putstr("Salut");
+  t_room salle;
+  FILE *fp;
+  char str[60];
+  int i = 0;
+  
+  fp = fopen("Maps/cargo_dock.map", "r");
+  if(fp == NULL)
+    {
+      my_putstr("Erreur: cargo_dock.map introuvable");
+      return 0;
+    }
+  while (fgets(str, 60, fp) != NULL)
+    {
+      salle.map[i] = str;
+      my_putstr(salle.map[i]);
+      i++;
+    }
+  fclose(fp);
+  
   return 0;
 }
